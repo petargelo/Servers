6b4330abebb36f5558592d9c9ae66baf3072f8c7
Petar Gelo <Petar.Gelo@intis.hr>
2021-05-24 16:25:10 +0200

    Fetching URL route parameters & editing User Test input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes=[
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UsersComponent},
   {path: 'servers', component: ServersComponent},
   {path: '', component: HomeComponent}
 ];
