16841d20be2c505999cdcbd8a4a5e631d0f4e809
Petar Gelo <pgelo@gelo.com>
2021-05-26 15:26:21 +0200

    Outsourcing Routing to app-routing.module instead of routing through app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,19 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+/* import { RouterModule, Routes } from '@angular/router';
+ */
 
-const appRoutes: Routes=[
+
+/* const appRoutes: Routes=[
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent, 
           children: [{path: ':id/:name', component: UserComponent}]},
   {path: 'servers', component: ServersComponent, 
           children: [{path: ':id', component: ServerComponent}, 
-                     {path: ':id/edit', component: EditServerComponent}]}
-  ];
+                     {path: ':id/edit', component: EditServerComponent}]}               
+  ]; */
 
 @NgModule({
   declarations: [
@@ -30,13 +33,14 @@ const appRoutes: Routes=[
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes) 
-  ],
+    AppRoutingModule /* u njemu se nalaze podaci za routing */
+/*     RouterModule.forRoot(appRoutes) 
+ */  ],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
